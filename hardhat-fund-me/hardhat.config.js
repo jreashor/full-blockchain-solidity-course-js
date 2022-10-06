f4216726b2b2add3d8c401e15fd564d783485c05
Jeffrey Reashor <jreashor@gmail.com>
2022-10-06 19:46:23 -0300

    Moving along................

---
+++ hardhat-fund-me/hardhat.config.js
@@ -0,0 +1,38 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+
+const ETH_GOERLI = process.env.ETH_GOERLI;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.9",
+    defaultNetwork: "hardhat",
+    networks: {
+        local: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+        goerli: {
+            url: ETH_GOERLI,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "CAD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        // token: "MATIC",
+    },
+};
